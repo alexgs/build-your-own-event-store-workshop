46d58ae978234eb3f2f5751aa383c6290c985ad4
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-08-17 17:36:08 -0400

    Initialize exercise 6

---
+++ exercises/exercise-06-stream-aggregation/src/get-events.test.ts
@@ -0,0 +1,67 @@
+import KnexClient, { Knex } from 'knex';
+import { v4 as uuid } from 'uuid';
+import { appendEvent } from './append-event';
+import { createStreamsTable } from './create-streams-table';
+import { createEventsTable } from './create-events-table';
+import { getEvents } from './get-events';
+
+describe('Function `getEvents`', () => {
+  let knex: Knex;
+
+  beforeAll(() => {
+    knex = KnexClient({
+      client: 'pg',
+      connection: {
+        host: 'localhost',
+        port: 5432,
+        user: 'postgres',
+        password: 'solarwinds123',
+      },
+      debug: false, // set to `true` to get useful debugging information in the console
+    });
+  });
+
+  afterAll(() => {
+    knex.destroy();
+  });
+
+  it('returns all events for the provided stream ID', async () => {
+    const STREAM_ID = uuid();
+    const STREAM_TYPE = 'test-events';
+
+    await createStreamsTable(knex);
+    await createEventsTable(knex);
+    await appendEvent(knex, {
+      id: uuid(),
+      data: { hello: 'Corrine' },
+      expectedVersion: 0,
+      streamId: STREAM_ID,
+      streamType: STREAM_TYPE,
+      type: 'test-event',
+    });
+    await appendEvent(knex, {
+      id: uuid(),
+      data: { hello: 'Carrie' },
+      expectedVersion: 1,
+      streamId: STREAM_ID,
+      streamType: STREAM_TYPE,
+      type: 'test-event',
+    });
+
+    const output = await getEvents(knex, STREAM_ID);
+
+    expect(output.length).toEqual(2);
+    expect(output[0]).toMatchObject({
+      data: { hello: 'Corrine' },
+      stream_id: STREAM_ID,
+      type: 'test-event',
+      version: '1',
+    });
+    expect(output[1]).toMatchObject({
+      data: { hello: 'Carrie' },
+      stream_id: STREAM_ID,
+      type: 'test-event',
+      version: '2',
+    });
+  });
+});
