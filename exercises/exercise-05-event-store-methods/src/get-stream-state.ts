762b6368d0f14f3dff8ce78557c1c1f79926f89f
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-08-17 17:27:14 -0400

    Implement `getStreamState` function

--- exercises/exercise-05-event-store-methods/src/get-stream-state.ts
+++ exercises/exercise-05-event-store-methods/src/get-stream-state.ts
@@ -1,3 +1,6 @@
 import { Knex } from 'knex';
 
-export async function getStreamState(knex: Knex, streamId: string) {}
+export async function getStreamState(knex: Knex, streamId: string) {
+  const result = await knex('streams').select().where('id', streamId);
+  return result[0];
+}
