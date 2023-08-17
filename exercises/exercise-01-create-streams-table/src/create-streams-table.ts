cc4060d98c63ae833fee0a6878fc15cacb607841
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-08-17 12:33:07 -0400

    Tweak table column tables

--- exercises/exercise-01-create-streams-table/src/create-streams-table.ts
+++ exercises/exercise-01-create-streams-table/src/create-streams-table.ts
@@ -3,7 +3,7 @@ import { Knex } from 'knex';
 export async function createStreamTable(knex: Knex) {
   await knex.schema.createTable('streams', (table) => {
     table.uuid('id').primary();
-    table.string('type');
-    table.increments('version');
+    table.text('type');
+    table.bigint('version');
   });
 }
