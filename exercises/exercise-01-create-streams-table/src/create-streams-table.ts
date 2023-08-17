0d8add20e3dcc7a0e13c0a34eeec0dfae5ad1bd5
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-08-17 12:26:26 -0400

    Get everything working!

--- exercises/exercise-01-create-streams-table/src/create-streams-table.ts
+++ exercises/exercise-01-create-streams-table/src/create-streams-table.ts
@@ -1,9 +1,9 @@
 import { Knex } from 'knex';
 
 export async function createStreamTable(knex: Knex) {
-  knex.schema.createTable('streams', (table) => {
+  await knex.schema.createTable('streams', (table) => {
     table.uuid('id').primary();
     table.string('type');
     table.increments('version');
-  }).toSQL();
+  });
 }
